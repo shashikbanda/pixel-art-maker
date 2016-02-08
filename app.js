13dbee6229d229a529d04e2162b3803c72dca32c
Shashik Banda <shashik.banda@gmail.com>
2016-02-07 20:55:49 -0800

    created and added css style sheet. created random color function and appened a 20% div for color selection pallet. 60% div easel under construction to fill out the rest of the space

--- app.js
+++ app.js
@@ -1,32 +1,47 @@
-// "use strict";
+// // "use strict";
 var body = document.getElementsByTagName('body')[0];
-var w = window.innerWidth;
-var h = window.innerHeight;
 
-var col = 40;
-var boxSize = w/40 - 10;
-
-var rows = Math.floor(h/boxSize) - 3;
+var colorPalletDiv = document.getElementsByClassName('colorpallet')[0];
+var easel = document.getElementsByClassName('easel')[0];
 
 
-function createSquare(color,classn){
+
+function createSquare(color,classn,divToAppendTo){
 	var square = document.createElement('div');
 	square.style.float = "left";
 	square.style.margin = "1px"
-	square.style.width = boxSize +'px';
-	square.style.height = boxSize + 'px';
+	square.style.width = colorPalletDiv.clientWidth/2 - 10+'px';
+	square.style.height = colorPalletDiv.clientWidth/2 - 10+ 'px';
 	square.style.border = 'solid';
 	square.style.backgroundColor = color;
 	square.className = classn;
-	body.appendChild(square);
+	divToAppendTo.appendChild(square);
 }
 
-for(var i = 0; i < col*rows - 20; i++){
-	createSquare('blue','square');
+
+function randomColorGenerator(){  //done
+	var color = "";
+	var randomColorNumber1 = Math.floor(Math.random() * (255 - 0 + 1)) + 0;
+	var randomColorNumber2 = Math.floor(Math.random() * (255 - 0 + 1)) + 0;
+	var randomColorNumber3 = Math.floor(Math.random() * (255 - 0 + 1)) + 0;
+
+	color = "rgb(" + randomColorNumber1 + "," + randomColorNumber2 + "," + randomColorNumber3 + ")"
+	return color; 
 }
 
-var gameSquares = document.getElementsByClassName('square');
-
-for(var i = 0; i < gameSquare.length; i ++){
-	
+function createColorPallet(){ //done
+	for(var k = 0; k < 14; k++){
+		var randomColor = randomColorGenerator();
+		createSquare(randomColor,'colorPalletDiv',colorPalletDiv);
+	}
 }
+
+function createEasel(){
+	for(var l = 0; l < 5; l++){
+		var currentColor = "green";
+		createSquare(currentColor,'easleDiv', easel)
+	}
+}
+
+createColorPallet();
+// createEasel();
