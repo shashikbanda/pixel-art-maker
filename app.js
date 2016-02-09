5d97040a66854b525e32eb8327cbfcc1f303b0e4
Shashik Banda <shashik.banda@gmail.com>
2016-02-08 21:22:28 -0800

    cleared out console logs. add comments to explain purpose of each function

--- app.js
+++ app.js
@@ -1,20 +1,16 @@
 // // "use strict";
 var body = document.getElementsByTagName('body')[0];
-
 var colorPalletDiv = document.getElementsByClassName('colorpallet')[0];
 var easel = document.getElementsByClassName('easel')[0];
-
 var currentColor = "white"
 
-function createSquare(color,classn,divToAppendTo){
+function createSquare(color,classn,divToAppendTo){ //GENERAL FUNCTION TO CREATE SQUARE
 	var square = document.createElement('div');
 	// console.log(easel)
 	// console.log(colorPalletDiv)
 	square.style.float = "left";
-	square.style.margin = "1px"
-	console.log(divToAppendTo)
+	square.style.margin = "1px";
 	if(divToAppendTo.className === 'col-md-2 colorpallet'){
-		console.log("sadf")
 		square.style.width = colorPalletDiv.clientWidth/2 - 20 +'px';
 		square.style.height = colorPalletDiv.clientWidth/2 - 20 + 'px';
 	}
@@ -22,17 +18,13 @@ function createSquare(color,classn,divToAppendTo){
 		square.style.width = easel.clientWidth/30 - 20+'px';
 		square.style.height = easel.clientWidth/30 - 20+ 'px';
 	}
-
-	console.log("easel div width = ", easel.clientWidth)
-	console.log("pallet div widrth = ", colorPalletDiv.clientWidth)
-	// square.style.border = 'solid';
 	square.style.backgroundColor = color;
 	square.className = classn;
 	divToAppendTo.appendChild(square);
 }
 
 
-function randomColorGenerator(){  //done
+function randomColorGenerator(){  //RANDOM COLOR GENERATOR -> SOON TO BE FIXED COLORS
 	var color = "";
 	var randomColorNumber1 = Math.floor(Math.random() * (255 - 0 + 1)) + 0;
 	var randomColorNumber2 = Math.floor(Math.random() * (255 - 0 + 1)) + 0;
@@ -42,39 +34,28 @@ function randomColorGenerator(){  //done
 	return color; 
 }
 
-function createColorPallet(){ //done
+function createColorPallet(){ //FUNCTION TO CREATE COLOR PALLET WITH 14 SQUARES
 	for(var k = 0; k < 14; k++){
 		var randomColor = randomColorGenerator();
 		createSquare(randomColor,'colorPalletDiv',colorPalletDiv);
 	}
 }
 
-function createEasel(){
+function createEasel(){ //FUNCTION TO CREATE EASEL WITH 100 SQUARES
 	for(var l = 0; l < 100; l++){ //2470 to fill grid
 		createSquare(currentColor,'easelDiv', easel)
 	}
 }
+createColorPallet(); //CALLS FUNCTION THAT GENRATES COLOR PALLET BOARD
+createEasel(); //CALLS FUNCTION THAT GENERATES EASEL BOARD
 
-$(function() {
-  $('#main-title').fadeIn( "slow", function() {
-  	console.log("jquery fade in success")
-  })
-});
-
-
-createColorPallet();
-createEasel();
-
-var colorL = document.getElementsByClassName('easelDiv');
-console.log(colorL.length)
-
+var colorL = document.getElementsByClassName('easelDiv'); //EVENT LISTENERS TO COLOR PALLET AND MAIN EASEL
 for(var m=0;m<colorL.length;m++){
 	colorL[m].addEventListener('mouseover',function(event){
 		event.target.style.backgroundColor = currentColor;
 	})
 }
 var palletSq = document.getElementsByClassName('colorPalletDiv');
-
 for(var n=0; n<palletSq.length;n++){
 	palletSq[n].addEventListener('click', function(event){
 		currentColor = event.target.style.backgroundColor
@@ -83,12 +64,10 @@ for(var n=0; n<palletSq.length;n++){
 }
 
 
-var resetButton = document.getElementById('reset-button');
-
+var resetButton = document.getElementById('reset-button'); //RESET BUTTON FUNCTIONALITY
 resetButton.addEventListener('click',function(){
 
 	for(var o=0; o < colorL.length; o++){
-		console.log("asdf")
 		console.log(colorL[o])
 		colorL[o].style.backgroundColor = "white"
 	}
